a7f352550ac7b569762cc4882f4b9b4c252e2650
washAcademico2 <washington.souza@academico@ufs.br>
2023-05-09 09:22:23 -0300

    Game Seed

--- src/main/java/com/example/intensivao/IntensivaoApplication.java
+++ src/main/java/com/example/intensivao/IntensivaoApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IntensivaoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IntensivaoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IntensivaoApplication.class, args);
+    }
 
 }
+
