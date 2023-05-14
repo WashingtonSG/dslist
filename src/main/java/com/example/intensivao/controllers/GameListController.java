dc85f6307bdeb5749c12881ab5f66f4723b3cebf
washAcademico2 <washington.souza@academico@ufs.br>
2023-05-14 10:21:08 -0300

    CORS config

--- src/main/java/com/example/intensivao/controllers/GameListController.java
+++ src/main/java/com/example/intensivao/controllers/GameListController.java
@@ -5,27 +5,29 @@ import com.example.intensivao.dto.GameMinDTO;
 import com.example.intensivao.services.GameListService;
 import com.example.intensivao.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/lists")
 public class GameListController {
+
     @Autowired
     private GameListService gameListService;
+
     @Autowired
     private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll() {
-        return (List<GameListDTO>) gameListService.findAll();
+        List<GameListDTO> result = gameListService.findAll();
+        return result;
     }
 
     @GetMapping(value = "/{listId}/games")
-    public List<GameMinDTO> findByList(@PathVariable Long listId) {
-        return gameService.findByGameList(listId);
+    public List<GameMinDTO> findGames(@PathVariable Long listId) {
+        List<GameMinDTO> result = gameService.findByGameList(listId);
+        return result;
     }
 }
